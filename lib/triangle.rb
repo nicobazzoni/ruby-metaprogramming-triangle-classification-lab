2f23f3203f06c6cce14fea7d915717b1c3103825
Nico Bazzoni <you@example.com>
2020-12-07 19:26:28 -0500

    commit tree be2764e25afb832c87f3f37b4c836dd80e85fbae

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,34 @@
-class Triangle
-  # write code here
+
+class Triangle 
+
+  attr_accessor :a, :b, :c 
+
+  def initialize(a, b, c)
+
+    @a = a
+    @b = b
+    @c = c
+  end
+
+ 
+ def kind
+  if a <= 0 || b <= 0 || c <= 0
+   raise TriangleError 
+  
+  elsif !((a + b) > c) || !((b + c) > a) || !((a + c) > b)
+    raise TriangleError 
+  end
+    
+    
+     return :equilateral if a == b && b == c
+    return :isosceles if a == b || b == c || a == c
+    :scalene
+  end
+
+   
+
+
+ class TriangleError < StandardError
+  
+ end
 end
